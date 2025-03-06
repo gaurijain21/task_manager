6f834c92a3ee82d79ab740d06247b9bc18a1db83
gaurijain21 <iamgaurijain@gmail.com>
2025-03-06 11:22:27 -0800

    Task Manager app is working!

--- app/tasks/page.js
+++ app/tasks/page.js
@@ -1,8 +1,9 @@
-export default function Tasks() {
+export default function Home() {
     return (
-        <div>
-            <h1>Task Manager</h1>
-            <p>Welcome to your task list!</p>
-        </div>
+      <div>
+        <h1>Welcome to the Task Manager</h1>
+        <p>Go to <a href="/tasks">Tasks Page</a></p>
+      </div>
     );
-}
+  }
+  
