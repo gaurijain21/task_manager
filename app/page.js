566285dc562cf4d72616756b678d376119a28918
gaurijain21 <iamgaurijain@gmail.com>
2025-03-12 15:21:29 -0700

    Updated js page

--- app/page.js
+++ app/page.js
@@ -7,26 +7,25 @@ export default function Home() {
         <i>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</i>
         Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      <br></br>
+      
       <p>This is pargraph 2. 
         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum </p>
 
-      <img src="/Downloads/ss.png" alt="screenshot of app" width="104" height="142"></img>
+      <img src="https://drive.google.com/file/d/1QaukAxt5wgpt55CF5mVNXtBFN5PDpzeZ/view?usp=drive_link" alt="screenshot of app" width="104" height="142"></img>
 
-      <p>
-        <ul>Features of app:
-          <li>managing tasks</li>
-          <li>feature 2</li>
-          <li>feature 3</li>
-        </ul>
-        <ol>
-          It can be used by:
-          <li>students</li>
-          <li>parents</li>
-          <li>teachers</li>
-        </ol>
-      </p>
+      <ul>Features of app:
+        <li>managing tasks</li>
+        <li>feature 2</li>
+        <li>feature 3</li>
+      </ul>
+      <ol>
+        It can be used by:
+        <li>students</li>
+        <li>parents</li>
+        <li>teachers</li>
+      </ol>
+      
       
 
       <p>Go to <a href="/tasks">Tasks Page</a></p>
